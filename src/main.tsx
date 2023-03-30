84959500f7c1516aec426a449c06bd413f91d96c
Fransedo Ravelino <fransedo.ravelino@BLUEBIRDGROUP.COM>
2023-03-30 12:28:32 +0700

    sample query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const GQL_URL = import.meta.env.VITE_GQL_URL;
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Intentionally not exposing the GraphQL URL, in case it is to be kept secret;
+const client = new ApolloClient({
+  uri: GQL_URL,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
