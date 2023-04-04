388d6a840eee7c985602d6dddbd9774cc3facc3c
Fransedo Ravelino <fransedo.ravelino@BLUEBIRDGROUP.COM>
2023-04-04 11:22:23 +0700

    added search contact

--- src/App.tsx
+++ src/App.tsx
@@ -10,33 +10,53 @@ import CreateEditContactDialog from "./components/CreateEditContactDialog";
 import { DELETE_CONTACT_BY_ID } from "./services/delete";
 
 function App() {
-  const localStorageFavorites = localStorage.getItem("favorites");
   const localStorageContacts = localStorage.getItem("contacts");
   const { loading, error, data, refetch } = useQuery(GET_CONTACT_LIST);
   const [deleteContactById] = useMutation(DELETE_CONTACT_BY_ID);
   const [searchValue, setSearchValue] = useState("");
   const [pageIndex, setPageIndex] = useState(0);
-  const [favoriteList, setFavoriteList] = useState<ContactListInterface[]>(
-    localStorageFavorites ? JSON.parse(localStorageFavorites) : []
-  );
+  const [favoriteList, setFavoriteList] = useState<ContactListInterface[]>([]);
   const [contactList, setContactList] = useState<ContactListInterface[]>(
     localStorageContacts ? JSON.parse(localStorageContacts) : []
   );
   const [openCreateContactDialog, setOpenCreateContactDialog] = useState(false);
 
+  const renderFavoriteList = useCallback(() => {
+    if (!searchValue) {
+      return favoriteList;
+    } else {
+      return favoriteList.filter((contact) =>
+        `${contact.first_name} ${contact.last_name}`
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      );
+    }
+  }, [searchValue, favoriteList]);
+
+  const renderContactList = useCallback(() => {
+    if (!searchValue) {
+      return contactList.slice(pageIndex * 10, (pageIndex + 1) * 10);
+    } else {
+      return contactList
+        .filter((contact) =>
+          `${contact.first_name} ${contact.last_name}`
+            .toLowerCase()
+            .includes(searchValue.toLowerCase())
+        )
+        .slice(pageIndex * 10, (pageIndex + 1) * 10);
+    }
+  }, [searchValue, contactList, pageIndex]);
+
   useEffect(() => {
     if (data) {
+      // Remove favorites from contact list
       setContactList(data.contact);
     }
   }, [data]);
 
   useEffect(() => {
-      localStorage.setItem("favorites", JSON.stringify(favoriteList));
-  }, [favoriteList]);
-
-	useEffect(() => {
-		localStorage.setItem("contacts", JSON.stringify(contactList));
-	}, [contactList])
+    localStorage.setItem("contacts", JSON.stringify(contactList));
+  }, [contactList]);
 
   const favoriteHandler = useCallback(
     (contact: ContactListInterface) => {
@@ -69,6 +89,7 @@ function App() {
           <div style={{ display: "flex", gap: "8px" }}>
             <input
               type="text"
+              aria-label="search-contacts"
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
             />
@@ -76,7 +97,7 @@ function App() {
           </div>
           <section>
             <SectionHeader>Favorites</SectionHeader>
-            {favoriteList.map((favContact) => (
+            {renderFavoriteList().map((favContact) => (
               <Contact
                 refetch={refetch}
                 contact={favContact}
@@ -89,7 +110,7 @@ function App() {
           </section>
           <section>
             <SectionHeader>Contact List</SectionHeader>
-            {contactList.slice(pageIndex * 10, (pageIndex + 1) * 10).map((contact) => (
+            {renderContactList().map((contact) => (
               <Contact
                 contact={contact}
                 key={contact.id}
@@ -99,7 +120,7 @@ function App() {
               />
             ))}
             <Pagination
-              totalPages={data.contact.length / 10}
+              totalPages={(data.contact.length - favoriteList.length) / 10}
               onBefore={() => {}}
               onNext={() => {}}
               onClickPage={pageClickHandler}
